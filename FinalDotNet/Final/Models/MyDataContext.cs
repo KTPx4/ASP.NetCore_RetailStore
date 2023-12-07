12a368274a9a71c91dba525aaedb0d54d0dba3f6
phuc mguyen <phucnguyenhoang3839@gmail.com>
2023-12-07 21:51:29 +0700

    version 1

---
+++ FinalDotNet/Final/Models/MyDataContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+using MongoDB.EntityFrameworkCore.Extensions;
+
+namespace Final.Models
+{
+    public class MyDataContext : DbContext
+    {
+     
+        public DbSet<Account> Accounts { get; set; }
+        public static MyDataContext Create(IMongoDatabase database) =>
+        new(new DbContextOptionsBuilder<MyDataContext>()
+            .UseMongoDB(database.Client, database.DatabaseNamespace.DatabaseName)
+            .Options);
+
+        public MyDataContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Account>().ToCollection("accounts");
+
+        }
+
+    }
+}
---
+++ FinalDotNet/Final/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Final.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Final.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private  MyDataContext _dbContext;
+
+        public IndexModel(MyDataContext myDContext)
+        {
+            _dbContext = myDContext;
+
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}
