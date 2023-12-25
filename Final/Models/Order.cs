aa4c20ab11b9b3334164bd1a127bd49c2d33d017
phuc mguyen <phucnguyenhoang3839@gmail.com>
2023-12-25 17:21:53 +0700

    finish the product section

---
+++ Final/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace Final.Models
+{
+    public class Order
+    {
+        public ObjectId Id { get; set; }
+
+        public string StaffEmail { get; set; }
+
+        public string CustomerPhone { get; set; }
+
+        public int ToltalPayment { get; set; }
+
+        public int MoneyGiven { get; set; }
+
+        public int MoneyExchange { get; set; }
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime CreateAt { get; set; }
+
+   
+    }
+}
--- Final/Pages/Product_Page/Index.cshtml.cs
+++ Final/Pages/Product_Page/Index.cshtml.cs
@@ -2,6 +2,7 @@ using Final.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace Final.Pages.Product_Page
 {
@@ -9,18 +10,33 @@ namespace Final.Pages.Product_Page
     {
         private readonly Final.Models.MyDataContext _context;
 
+        public bool IsAdmin { get; private set; }
+
+        public string search = "";
+
         public IndexModel(Final.Models.MyDataContext context)
         {
+            
             _context = context;
         }
 
         public IList<Product> Product { get; set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string? search)
         {
+
+
+            IQueryable<Product> query = _context.Products;
+            string role = HttpContext.User.FindFirst(ClaimTypes.Role)?.Value;
+            if (search != null)
+            {
+                this.search = search;
+                query = query.Where(p => p.ProductName.Contains(search) || p.BarCode.Contains(search));
+            }
+            IsAdmin = role?.ToLower() == "admin";
             if (_context.Products != null)
             {
-                Product = await _context.Products.ToListAsync();
+                Product = query.OrderByDescending(p => p.ProductName).ToList();
             }
         }
      
